f3a32e0a93b84dda8447d5c959056405d0d9894a
elliottwu <elliottwu@users.noreply.github.com>
2022-12-07 01:26:37 +0300

    updated links

--- datasets/download_dataset.sh
+++ datasets/download_dataset.sh
@@ -1,7 +1,7 @@
 # celeba dataset
 download_celeba(){
     echo "----------------------- downloading celeba dataset -----------------------"
-    wget -O celeba.tar.gz "https://www.robots.ox.ac.uk/~szwu/storage/sketch/celeba.tar.gz"
+    wget -O celeba.tar.gz "https://www.robots.ox.ac.uk/~szwu/storage/18_sketch/celeba.tar.gz"
     tar xzvf celeba.tar.gz -C ./datasets/
     rm celeba.tar.gz
 }
--- download_pretrained_model.sh
+++ download_pretrained_model.sh
@@ -1,7 +1,7 @@
 # face model
 download_face(){
     echo "----------------------- downloading face pretrained model -----------------------"
-    wget -O face_pretrained.tar.gz "https://www.robots.ox.ac.uk/~szwu/storage/sketch/face_pretrained.tar.gz"
+    wget -O face_pretrained.tar.gz "https://www.robots.ox.ac.uk/~szwu/storage/18_sketch/face_pretrained.tar.gz"
     tar xzvf face_pretrained.tar.gz
     rm face_pretrained.tar.gz
 }
